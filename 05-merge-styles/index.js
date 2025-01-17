f496c8a57628c692cfd43176cc0ef6fed503f435
AnnaSergeevnaIv <kara4eva.anya@gmail.com>
2025-01-17 09:15:47 +0300

    style: solve lint errors

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
-const startPath = path.join(__dirname, "styles");
-const writeStream = fs.createWriteStream(path.join(__dirname, "project-dist", "bundle.css"))
+const startPath = path.join(__dirname, 'styles');
+const writeStream = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+);
 
 let styles = [];
 
-fs.promises.readdir(startPath)
-  .then((files) =>  {
-      let promises = files.map((file) => {
-        const filePath = path.join(startPath, file);
-        if (path.extname(file) === ".css") 
-          return fs.promises.readFile(filePath, "utf-8").then((data) => styles.push(data))
-      })
-      return Promise.all(promises);
-    })
-    .then(() => writeStyles())
+fs.promises
+  .readdir(startPath)
+  .then((files) => {
+    let promises = files.map((file) => {
+      const filePath = path.join(startPath, file);
+      if (path.extname(file) === '.css')
+        return fs.promises
+          .readFile(filePath, 'utf-8')
+          .then((data) => styles.push(data));
+    });
+    return Promise.all(promises);
+  })
+  .then(() => writeStyles());
 
 function writeStyles() {
-  let entireStyle = styles.join("");
-  console.log(styles)
-  writeStream.write(entireStyle)
+  let entireStyle = styles.join('');
+  console.log(styles);
+  writeStream.write(entireStyle);
 }
-
